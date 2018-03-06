c07f64c2b15dcbd7d8226cb78dac2006ce8e26be
InerstIO <inerstio@gmail.com>
2018-03-05 20:30:10 -0800

    add direct str input

--- word_frequencies.py
+++ word_frequencies.py
@@ -4,11 +4,13 @@ from collections import Counter
 from collections import defaultdict
 
 class Text:
-	def __init__(self, path):
-		with open(path, 'r') as f:
-			#\W == [^a-zA-Z0-9_], which excludes all numbers, letters and _
-			self.s = re.sub(r'[\W_]+', ' ', f.read().replace("'",'')).lower()	#may use read(size) to avoid OOM for large inputs
-
+	def __init__(self, path, s = None):
+		if not s:
+			with open(path, 'r') as f:
+				#\W == [^a-zA-Z0-9_], which excludes all numbers, letters and _
+				self.s = re.sub(r'[\W_]+', ' ', f.read().replace("'",'')).lower()	#may use read(size) to avoid OOM for large inputs
+		else:
+			self.s = re.sub(r'[\W_]+', ' ', s.replace("'",'')).lower()
 	def tokenize(self):
 		return self.s.split()
 
