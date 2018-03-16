f005ea746a279db7ac8626ce139e3d3ed3c416c2
InerstIO <inerstio@gmail.com>
2018-03-16 15:52:53 -0700

    split fetching from google to a new script

---
+++ get_gresults.py
@@ -0,0 +1,31 @@
+from googlesearch.googlesearch import GoogleSearch
+# written by Anthony Hseb and made compatible to py3 by notmikeb
+# add more delay by myself to avoid request denial
+from urllib.parse import urlparse
+import json
+
+def strip_scheme(url):
+# cited from https://stackoverflow.com/questions/21687408/how-to-remove-scheme-from-url-in-python
+    parsed = urlparse(url)
+    scheme = "%s://" % parsed.scheme
+    return parsed.geturl().replace(scheme, '', 1)
+
+def clean_url(url):
+	ret = strip_scheme(url)
+	return ret if ret[-1] != '/' else ret[:-1]
+
+def google(keywords):
+	response = GoogleSearch().search(keywords + "site:ics.uci.edu", num_results = 100, prefetch_pages = False)
+	url = [result.url for result in response.results]
+	url = list(map(clean_url, url))
+
+	return [link for link in url if not link.endswith('.pdf')]
+
+if __name__ == '__main__':
+	query = ['mondego', 'machine learning', 'software engineering', 'security', 'student affairs', 'graduate courses', \
+			'Crista Lopes', 'REST', 'computer games', 'information retrieval']
+	d={}
+	for keywords in query:
+		d[keywords] = google(keywords)
+		with open('gresults2.json', 'w') as f:
+			f.write(json.dumps(d))
--- ndcg.py
+++ ndcg.py
@@ -1,29 +1,12 @@
-from googlesearch.googlesearch import GoogleSearch
-# written by Anthony Hseb and made compatible to py3 by notmikeb
-# add more delay by myself to avoid request denial
 import math
 import query as qry
-from urllib.parse import urlparse
+import json
 
 tier = [5,10] # top 5: 3, top 10: 2, rest in google result: 1, not in google result: 0
 N = 5
 
-def strip_scheme(url):
-# cited from https://stackoverflow.com/questions/21687408/how-to-remove-scheme-from-url-in-python
-    parsed = urlparse(url)
-    scheme = "%s://" % parsed.scheme
-    return parsed.geturl().replace(scheme, '', 1)
-
 def clean_url(url):
-	ret = strip_scheme(url)
-	return ret if ret[-1] != '/' else ret[:-1]
-
-def google(keywords):
-	response = GoogleSearch().search(keywords + "site:ics.uci.edu", num_results = 100, prefetch_pages = False)
-	url = [result.url for result in response.results]
-	url = list(map(clean_url, url))
-
-	return [link for link in url if not link.endswith('.pdf')]
+	return url if url[-1] != '/' else url[:-1]
 
 def dcg(gresults, myresults, perfect=False):
 	scores = []
@@ -53,9 +36,11 @@ if __name__ == '__main__':
 	query = ['mondego', 'machine learning', 'software engineering', 'security', 'student affairs', 'graduate courses', \
 			'Crista Lopes', 'REST', 'computer games', 'information retrieval']
 	se = qry.search_engine()
+	with open('gresults.json', 'r') as f:
+		d = json.loads(f.read())
 	for keywords in query:
-		g = google(keywords)
-		my = se.query(keywords)[0]
+		g = d[keywords]
+		my = list(map(clean_url, se.query(keywords)[0]))
 		print(g)
 		print(my)
 		print(keywords + ': ' + str(ndcg(g, my)))
